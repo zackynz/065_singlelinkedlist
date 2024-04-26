3cf769aedf862a32f030759432a49e8ef8aa9b0d
zackynz <nurasfanz@gmail.com>
2024-04-26 16:24:05 +0800

    menambahkan simbol pada data

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -113,4 +113,56 @@ void searchData() {
         system("cls");
         return;
     }
+    else {
+        int nim;
+        cout << "Masukkan NIM: ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMHS == nim) {
+                cout << "NIM: " << currentNode->noMHS << ", Nama: " << currentNode->name << endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
+}
+
+int main() {
+    int pilihan;
+    do
+    {
+        try
+        {
+
+            cout << "1. Tambah Data" << endl;
+            cout << "2. Hapus Data" << endl;
+            cout << "3. Tampilkan Data" << endl;
+            cout << "4. Cari Data" << endl;
+            cout << "5. Keluar" << endl;
+            cout << "Pilihan: ";
+            cin >> pilihan;
+            switch (pilihan)
+            {
+                case 1:
+                    addNode();
+                    cout << "Data Berhasil Ditambahkan" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                case 2:
+                    if (listEmpety())
+                    {
+                        cout << "List Kosong" << endl;
+                        system("pause");
+                        system("cls");
+                        break;
+                    }
+
+
+                    }
+            }
+        }
+    }
 }
