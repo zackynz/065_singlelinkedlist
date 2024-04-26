fc426d040c61b5d395df211d7a7ffc6d774178cc
zackynz <nurasfanz@gmail.com>
2024-04-26 15:59:17 +0800

    memberikan daftar kosong

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -82,3 +82,10 @@ bool deleteNode(int nim) {
         START = current->next;
     return true;
 }
+bool listEmpety() {
+    if (START == NULL)
+        return true;
+    else
+        return false;
+        
+}
