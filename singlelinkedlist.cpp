84dc782ae2d9b4ecf4db46ada0f6916a6dc82975
zackynz <nurasfanz@gmail.com>
2024-04-26 16:11:11 +0800

    memberikan perintah untuk tidak mengulang data dan melakukan pencarian data

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -97,5 +97,20 @@ void traverse() {
         system("cls");
         return;
     }
+    else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << "NIM: " << currentNode->noMHS << ", Nama: " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }  
+}
+
+void searchData() {
+    if (listEmpety()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
     }
 }
