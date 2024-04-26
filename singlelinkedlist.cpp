89784803b0df312b8955bd45cec45a2a54782f95
zackynz <nurasfanz@gmail.com>
2024-04-26 16:29:23 +0800

    end

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -160,9 +160,33 @@ int main() {
                         break;
                     }
 
-
+                    int nim;
+                    cout << "Masukkan NIM: ";
+                    cin >> nim;
+                    if (deleteNode(nim)) {
+                        cout << "nim: " << nim << " berhasil dihapus" << endl;
+                        system("pause");
+                        system("cls");
                     }
+                    else
+                        cout << "Data tidak ditemukan" << endl;
+                    break;
+                case 3:
+                    traverse();
+                    break;
+                case 4:
+                    searchData();
+                    break;
+                case 5:
+                    break;
+                default:
+                    cout << "Pilihan tidak ada" << endl;
+                    break;
             }
         }
-    }
+        catch (exception e)
+        {
+            cout << "Terjadi Kesalahan" << endl;
+        }
+    }while (pilihan != 5);
 }
